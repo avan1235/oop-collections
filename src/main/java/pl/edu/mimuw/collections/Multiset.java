6fa146709c951e5444df0c9787a5b05a0c34791c
Scharmi <scharmi1500@gmail.com>
2022-06-01 22:16:57 +0200

    Finished Homework

---
+++ src/main/java/pl/edu/mimuw/collections/Multiset.java
@@ -0,0 +1,132 @@
+package pl.edu.mimuw.collections;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+public class Multiset<E> implements IMultiset<E>, Iterable<E> {
+    
+    private Map<E, Integer> occurencesMap;
+    private int size;
+
+    public Multiset() {
+        this.size = 0;
+        this.occurencesMap = new HashMap<>();
+    }   
+    public int size() {
+        return size;
+    }
+    
+    public int count(E element) {
+        if(occurencesMap.containsKey(element)) {
+            return occurencesMap.get(element);
+        }
+        return 0;
+    }
+
+    public int add(E element, int occurrences) {
+        if(!occurencesMap.containsKey(element)) {
+            occurencesMap.put(element, occurrences);
+            return occurrences;
+        }
+        int prevOccurences = occurencesMap.get(element);
+        occurencesMap.put(element, occurrences + prevOccurences);
+        return occurrences + prevOccurences;
+    }
+
+    public boolean add(E element) {
+        if(!occurencesMap.containsKey(element)) {
+            occurencesMap.put(element, 1);
+            return true;
+        }
+        int prevOccurences = occurencesMap.get(element);
+        occurencesMap.put(element, prevOccurences + 1);
+        return true;
+    }
+
+    public boolean remove(E element) {
+        if(!occurencesMap.containsKey(element)) {
+            return false;
+        }
+        int prevOccurences = occurencesMap.get(element);
+        occurencesMap.put(element, Math.max(0, prevOccurences - 1));
+        return (Math.max(0, prevOccurences - 1) > 0);
+    }
+
+    public int remove(E element, int occurrences) {
+        if(!occurencesMap.containsKey(element)) {
+            return 0;
+        }
+        int prevOccurences = occurencesMap.get(element);
+        occurencesMap.put(element, Math.max(0, prevOccurences - occurrences));
+        return Math.max(0, prevOccurences - occurrences);
+    }
+
+    public Set<E> elementSet() {
+        Set<E> s = new HashSet<>();
+        for (Map.Entry<E, Integer> entry : occurencesMap.entrySet()) {
+            if(entry.getValue() != 0) s.add(entry.getKey());
+        }
+        return s;
+    }
+
+    public boolean contains(E element) {
+        if(!occurencesMap.containsKey(element)) {
+            if(occurencesMap.get(element) != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    @Override
+    public boolean equals(Object object) {
+        if(object.getClass() == this.getClass()) {
+            return object.hashCode() == this.hashCode();
+        }
+        return false;
+    }
+
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            Iterator<Map.Entry<E, Integer>> mapEntryIterator = occurencesMap.entrySet().iterator();
+            int remainingOccurences = 0;
+            Map.Entry<E, Integer> currentElement;
+            public boolean hasNext() {
+                if(remainingOccurences <= 0) return mapEntryIterator.hasNext();
+                return true;
+            }
+            public E next() {
+                if(remainingOccurences <= 0) {
+                    currentElement = mapEntryIterator.next();
+                    remainingOccurences = Multiset.this.count(currentElement.getKey());
+                }
+                remainingOccurences--;
+                return currentElement.getKey();
+            }
+        };
+    }
+  
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.toString(), this.getClass());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("Multiset:\n{\n");
+        for(Map.Entry<E, Integer> entry : occurencesMap.entrySet()) {
+            if(entry.getValue() != 0) {
+                s.append(entry.getKey() + ": " + entry.getValue() + "\n");
+            }
+        }
+        s.append("}");
+        return s.toString();
+    }
+}
--- src/test/java/pl/edu/mimuw/collections/MultisetTest.java
+++ src/test/java/pl/edu/mimuw/collections/MultisetTest.java
@@ -4,6 +4,67 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MultisetTest {
+import java.util.HashSet;
 
+class MultisetTest {
+    @Test 
+    void addingAndRemovingSingleElement() {
+        var multiSet = new Multiset<Integer>();
+        multiSet.add(1);
+        multiSet.add(1);
+        multiSet.add(1);
+        multiSet.add(2);
+        multiSet.add(1);
+        multiSet.add(2);
+        multiSet.add(3);
+        multiSet.remove(4);
+        multiSet.remove(2);
+        multiSet.remove(1);
+        multiSet.remove(1);
+        multiSet.remove(3);
+        multiSet.remove(3);
+        assertEquals(multiSet.count(1), 2);
+        assertEquals(multiSet.count(2), 1);
+        assertEquals(multiSet.count(3), 0);
+        assertEquals(multiSet.count(490), 0);
+    }
+
+    @Test 
+    void addingAndRemovingMultipleElements() {
+        var multiSet = new Multiset<Integer>();
+        multiSet.add(2,1);
+        multiSet.add(1,5);
+        multiSet.add(2,1);
+        multiSet.add(3,7);
+        multiSet.add(1,3);
+        multiSet.remove(1,6);
+        multiSet.remove(1);
+        multiSet.remove(3,20);
+        assertEquals(multiSet.count(1), 1);
+        assertEquals(multiSet.count(2), 2);
+        assertEquals(multiSet.count(3), 0);
+        assertEquals(multiSet.count(490), 0);
+    }
+    @Test
+    void equalsTest() {
+        var multiSet1 = new Multiset<Integer>();
+        var multiSet2 = new Multiset<Integer>();
+        var multiSet3 = new Multiset<Integer>();
+        var multiSet4 = new Multiset<Integer>();
+        var multiSet5 = new Multiset<Integer>();
+        var set = new HashSet<Integer>();
+        multiSet1.add(1,1);
+        multiSet2.add(1,1);
+        multiSet3.add(1,2);
+        multiSet4.add(1,1);
+        multiSet4.add(2,1);
+        multiSet5.add(1,3);
+        multiSet5.remove(1,2);
+        set.add(1);
+        assertEquals(multiSet1.equals(multiSet2), true);
+        assertEquals(multiSet2.equals(multiSet5), true);
+        assertEquals(multiSet3.equals(multiSet2), false);
+        assertEquals(multiSet1.equals(multiSet4), false);
+        assertEquals(multiSet1.equals(set), false);
+    }
 }
