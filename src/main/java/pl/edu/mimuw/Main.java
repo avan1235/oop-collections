e294bc32230b001443bc591e3d85edb61d0e590a
jszot <j.szot@student.uw.edu.pl>
2022-06-20 20:01:28 +0200

    zgodnie z sugestią, wysyłam zaległe collections

--- src/main/java/pl/edu/mimuw/Main.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.mimuw;
-
-public class Main {
-
-  public static void main(String[] args) {
-    throw new IllegalStateException("TODO: show your toString() implementation on simple examples");
-  }
-}
--- src/test/java/pl/edu/mimuw/collections/MultisetTest.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.mimuw.collections;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class MultisetTest {
-
-}
